dae0af2cb31965ae69bd0f8d4854d38c0a6b3a43
prebz89 <prebengunders1@gmail.com>
2018-08-21 17:55:28 +0200

    My project setup completed successfully.

---
+++ shoppingbackend/src/main/java/net/prebz/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.prebz.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
